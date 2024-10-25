6831934caf7cabf42f5220d3ded98f96dabe2264
Abdelrehman <abdelrehmanmamdouh@gmail.com>
2024-10-25 15:05:50 +0300

    chore(config): Init Mongoose and swagger configuration.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
@@ -9,6 +10,7 @@ import { AppService } from './app.service';
       envFilePath: `.env`,
       isGlobal: true,
     }),
+    MongooseModule.forRoot(process.env.DATABASE_URL),
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,48 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
 
   app.setGlobalPrefix('api/v0');
 
   const configService = new ConfigService();
 
   const port = configService.get('PORT') || 8080;
+
+  const config = new DocumentBuilder()
+    .addBearerAuth(undefined, 'default')
+    .setTitle('Bitsonomy Assessment')
+    .setDescription('The Bitsonomy Assessment API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: {
+      authAction: {
+        default: {
+          name: 'default',
+          schema: {
+            description: 'Default',
+            type: 'http',
+            in: 'header',
+            scheme: 'bearer',
+            bearerFormat: 'JWT',
+          },
+          value: configService.get('TOKEN'),
+        },
+      },
+    },
+  });
 
   await app.listen(port);
 }
