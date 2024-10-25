2d3f49ef9beec45a40cc0950bd6609868df954c2
Abdelrehman <abdelrehmanmamdouh@gmail.com>
2024-10-25 14:51:22 +0300

    chore(config): Initialize app and configs.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.setGlobalPrefix('api/v0');
+
+  const configService = new ConfigService();
+
+  const port = configService.get('PORT') || 8080;
+
+  await app.listen(port);
 }
 bootstrap();
