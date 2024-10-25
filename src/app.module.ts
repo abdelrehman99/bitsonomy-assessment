b21a287839a08443791155af56c6a20cebe7c23d
Abdelrehman <abdelrehmanmamdouh@gmail.com>
2024-10-25 21:50:11 +0300

    chore(config): Add redis configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { RedisOptions } from './configs/redis.config';
+import { ConfigModule } from '@nestjs/config';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
   imports: [
@@ -13,6 +15,7 @@ import { AuthModule } from './auth/auth.module';
       isGlobal: true,
     }),
     MongooseModule.forRoot(process.env.DATABASE_URL),
+    CacheModule.registerAsync(RedisOptions),
     UsersModule,
     AuthModule,
   ],
